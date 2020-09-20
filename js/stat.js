612f648f7c6355f560cb2cbb29fac8cd18b32f80
montasir <montasir1400@gmail.com>
2020-09-20 17:16:25 +0300

    Белое облако с координатами [100, 10] высотой 270px и шириной 420px

--- js/stat.js
+++ js/stat.js
@@ -1,22 +1,22 @@
-"use strict";
+'use strict';
 
 window.renderStatistics = function (ctx) {
   ctx.strokeStyle = `#000000`;
   ctx.lineWidth = 1;
-  ctx.strokeRect(110, 20, 420, 266);
+  ctx.strokeRect(110, 20, 420, 269);
 
-  ctx.strokeStyle = `rgba(0, 0, 0, 0.6)`;
+  ctx.strokeStyle = `rgba(0, 0, 0, 0.7)`;
   ctx.lineWidth = 10;
-  ctx.strokeRect(115, 25, 410, 257);
+  ctx.strokeRect(115, 25, 410, 260);
 
-  ctx.strokeStyle = `#36499c`;
-  ctx.lineWidth = 1;
-  ctx.strokeRect(99, 11, 421, 266);
+  ctx.fillStyle = `#ffffff`;
+  ctx.fillRect(100, 10, 420, 270);
 
   ctx.strokeStyle = `#aeb4cd`;
   ctx.lineWidth = 1;
-  ctx.strokeRect(100, 12, 420, 265);
+  ctx.strokeRect(99, 9, 421, 271);
 
-  ctx.fillStyle = `#ffffff`;
-  ctx.fillRect(100, 12, 420, 265);
+  ctx.strokeStyle = `#36499c`;
+  ctx.lineWidth = 1;
+  ctx.strokeRect(98, 8, 423, 273);
 };
