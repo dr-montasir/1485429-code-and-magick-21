8fc1f63da6e4202db0b2a73810f4ad349baf7eee
montasir <montasir1400@gmail.com>
2020-09-26 03:12:27 +0300

    добавил COAT_COLOR & EYES_COLOR

--- js/setup.js
+++ js/setup.js
@@ -33,22 +33,22 @@ const WIZARD_SURNAMES = [
   `Ирвинг`
 ];
 
-// const COAT_COLOR = [
-//   `rgb(101, 137, 164)`,
-//   `rgb(241, 43, 107)`,
-//   `rgb(146, 100, 161)`,
-//   `rgb(56, 159, 117)`
-//   `rgb(215, 210, 55)`,
-//   `rgb(0, 0, 0)`
-// ];
+const COAT_COLOR = [
+  `rgb(101, 137, 164)`,
+  `rgb(241, 43, 107)`,
+  `rgb(146, 100, 161)`,
+  `rgb(56, 159, 117)`,
+  `rgb(215, 210, 55)`,
+  `rgb(0, 0, 0)`
+];
 
-// const EYES_COLOR = [
-//   `black`,
-//   `red`,
-//   `blue`,
-//   `yellow`,
-//   `green`
-// ];
+const EYES_COLOR = [
+  `black`,
+  `red`,
+  `blue`,
+  `yellow`,
+  `green`
+];
 
 const NUMBER_OF_WIZARDS = 4;
 
@@ -56,6 +56,8 @@ for (let i = 0; i < NUMBER_OF_WIZARDS; i++) {
   const wizardElement = similarWizardTemplate.cloneNode(true);
 
   wizardElement.querySelector(`.setup-similar-label`).textContent = WIZARD_NAMES[i] + ` ` + WIZARD_SURNAMES[i];
+  wizardElement.querySelector(`.wizard-coat`).style.fill = COAT_COLOR[i];
+  wizardElement.querySelector(`.wizard-eyes`).style.fill = EYES_COLOR[i];
 
   similarListElement.appendChild(wizardElement);
 }
