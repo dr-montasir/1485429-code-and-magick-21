a2414b499efa59406430b2b02690ee6839ac8085
montasir <montasir1400@gmail.com>
2020-10-05 13:40:37 +0300

    Учебный проект: одеть Надежду - часть 1

--- js/setup.js
+++ js/setup.js
@@ -96,3 +96,46 @@ const renderWizards = (wizards) => {
 };
 
 similarListElement.appendChild(renderWizards(getWizards()));
+
+const setupOpen = document.querySelector(`.setup-open`);
+const setup = document.querySelector(`.setup`);
+const setupClose = setup.querySelector(`.setup-close`);
+
+const onPopupEscPress = (evt) => {
+  if (evt.key === `Escape`) {
+    evt.preventDefault();
+    closePopup();
+  }
+};
+
+const openPopup = () => {
+  setup.classList.remove(`hidden`);
+
+  document.addEventListener(`keydown`, onPopupEscPress);
+};
+
+const closePopup = () => {
+  setup.classList.add(`hidden`);
+
+  document.removeEventListener(`keydown`, onPopupEscPress);
+};
+
+setupOpen.addEventListener(`click`, () => {
+  openPopup();
+});
+
+setupOpen.addEventListener(`keydown`, (evt) => {
+  if (evt.key === `Enter`) {
+    openPopup();
+  }
+});
+
+setupClose.addEventListener(`click`, () => {
+  closePopup();
+});
+
+setupClose.addEventListener(`keydown`, (evt) => {
+  if (evt.key === `Enter`) {
+    closePopup();
+  }
+});
