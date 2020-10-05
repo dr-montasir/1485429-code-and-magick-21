68fa73eefc322d4592e53d85ddd8a49ba4e177c8
montasir <montasir1400@gmail.com>
2020-10-05 20:59:15 +0300

    предыдущая комментария про фрагмент касается другой проект

--- js/setup.js
+++ js/setup.js
@@ -97,6 +97,7 @@ const renderWizards = (wizards) => {
 
 similarListElement.appendChild(renderWizards(getWizards()));
 
+// module4-task1
 const setupOpen = document.querySelector(`.setup-open`);
 const setup = document.querySelector(`.setup`);
 const setupClose = setup.querySelector(`.setup-close`);
